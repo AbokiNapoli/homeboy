4559f2119d8f8883eed900faf05c76db9914361c
napoli <abokivvu@gmail.com>
2024-03-01 18:30:10 +0000

    route fix1

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -8,7 +8,6 @@
     <!-- Google fonts -->	
     <link rel="preconnect" href="fonts.googleapis.com/index.html">
     <link rel="preconnect" href="fonts.gstatic.com/index.html" crossorigin>
-    <link href="fonts.googleapis.com/css20c56.css?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,600;1,700;1,800&amp;family=Poppins:wght@400;600;700;800;900&amp;family=Roboto:wght@400;500;700;900&amp;display=swap" rel="stylesheet">
     
     <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
