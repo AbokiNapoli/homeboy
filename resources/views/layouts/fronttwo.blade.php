80124ccfcc92d3989b6d0f1cc6220bfa4e041b6d
napoli <abokivvu@gmail.com>
2024-03-01 17:18:40 +0000

    render push6

--- resources/views/layouts/fronttwo.blade.php
+++ resources/views/layouts/fronttwo.blade.php
@@ -32,17 +32,7 @@
 	<body class="home-1">
 
 		<div class="preloader">
-			<dotlottie-player 
-				src="https://lottie.host/d851f1dc-6ef5-40be-8ca8-bbffcd018276/wutdxEizj0.json" 
-				background="#04000A" 
-				speed="1" 
-				style="width: 100%; height: 100%" 
-				direction="1" 
-				mode="normal" 
-				loop 
-				autoplay>
-			</dotlottie-player>
-			<script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+			<img src="assets/images/global/bra-homeboy1.jpg" class="logo-custom-dark" alt="logo"> 
 		</div>
 
 		<!-- Header ======================-->	
