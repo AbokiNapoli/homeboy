11067c0c39f0d1c00652ae24a5b40b144bf21b4a
napoli <abokivvu@gmail.com>
2024-03-01 17:36:21 +0000

    render push7

--- resources/views/layouts/fronttwo.blade.php
+++ resources/views/layouts/fronttwo.blade.php
@@ -13,7 +13,6 @@
 		<!-- Google fonts -->	
 		<link rel="preconnect" href="fonts.googleapis.com/index.html">
 		<link rel="preconnect" href="fonts.gstatic.com/index.html" crossorigin>
-		<link href="fonts.googleapis.com/css23251.css?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,600;1,700;1,800&amp;family=Plus+Jakarta+Sans:wght@400;500;600;700;800;900&amp;family=Poppins:wght@600;800&amp;family=Roboto:wght@400;500;700;900&amp;display=swap" rel="stylesheet"> 
 		
 		<link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
 		<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
